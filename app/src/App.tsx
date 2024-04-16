abfca486e0ccc293eb3284a9f42564b8087675e1
kasapvictor <kasap.victor@gmail.com>
2024-04-15 18:23:33 -0700

    feat: Added App add function and Button

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ChangeEvent, useState } from 'react';
+import { Button } from "@mono/ui";
+import { add } from "@mono/utils";
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [nums, setNums] = useState({a: '', b: ''})
 
+    const handleNumChange = (key: keyof typeof nums) => (e: ChangeEvent<HTMLInputElement>) => {
+      setNums((prevState) => ({
+          ...prevState,
+          [key]: e.target.value,
+      }));
+    }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+        <input type="text" value={nums.a} onChange={handleNumChange('a')}/>
+        <input type="text" value={nums.b} onChange={handleNumChange('b')}/>
+        <Button onClick={() => {
+            alert(add(Number(nums.a), Number(nums.b)));
+        }}>Add</Button>
+    </div>
   )
 }
 
