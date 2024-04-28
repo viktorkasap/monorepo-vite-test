78a2fe1a5d80683c4d96765a4390398d95f90b85
kasapvictor <kasap.victor@gmail.com>
2024-04-28 14:12:30 -0700

    feat: Added husky and lint-staged

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,11 +1,3 @@
-// module.exports = {
-//   '**/src/**/*.{js,jsx,ts,tsx,json}': [
-//     'prettier --write app packages',
-//     'eslint \'app/src/**/*.{ts,tsx}\' \'packages/*/src/**/*.{ts,tsx}\' --fix',
-//     () => 'tsc --noEmit', // показывает ошибки но в терминале нет ссылки на файл с ошибкой
-//   ],
-//   '**/src/**/*.{css,scss}': ['pnpm lint:style'],
-// };
 module.exports = {
   '**/*.{js,jsx,ts,tsx,json}': [
     'pnpm pretty',
