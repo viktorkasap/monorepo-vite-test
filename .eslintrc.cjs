07e78ba6133255398c84986d753ae36f9df22c4e
kasapvictor <kasap.victor@gmail.com>
2024-04-28 11:13:37 -0700

    docs: Updated README.md

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,11 +9,13 @@ module.exports = {
   ignorePatterns: ['dist', 'node_modules', '.eslintrc.cjs'],
 
   extends: [
+    'eslint:recommended',
     'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
     'standard',
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
     'prettier',
     'plugin:prettier/recommended',
   ],
